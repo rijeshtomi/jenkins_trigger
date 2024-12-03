366e71d1bc4c96f2555611b9b556960c07a471eb
Rijesh Tomi <rtomi@cloudbees.com>
2024-12-03 15:16:29 +0530

    adding maven project and jenkins file

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("test1.txt");
+        System.out.println("Hello world!");
+    }
+}
